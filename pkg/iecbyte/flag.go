f67c8116d623a236156077d4001fd1ee5cb15522
Andrew Heberle <andrew.heberle@gmail.com>
2025-03-31 16:35:52 +0800

    Update tests

--- pkg/iecbyte/flag.go
+++ pkg/iecbyte/flag.go
@@ -11,27 +11,34 @@ type Flag struct {
 	n int64
 }
 
-var (
-	multipliers = map[string]int64{
-		"Ei": 1024 * 1024 * 1024 * 1024 * 1024 * 1024,
-		"Pi": 1024 * 1024 * 1024 * 1024 * 1024,
-		"Ti": 1024 * 1024 * 1024 * 1024,
-		"Gi": 1024 * 1024 * 1024,
-		"Mi": 1024 * 1024,
-		"Ki": 1024,
-	}
-	order = []string{"Ei", "Pi", "Ti", "Gi", "Mi", "Ki"}
-)
+type multiplier struct {
+	Suffix string
+	Value  int64
+}
 
+var multipliers = []multiplier{
+	{"Ei", 1024 * 1024 * 1024 * 1024 * 1024 * 1024},
+	{"Pi", 1024 * 1024 * 1024 * 1024 * 1024},
+	{"Ti", 1024 * 1024 * 1024 * 1024},
+	{"Gi", 1024 * 1024 * 1024},
+	{"Mi", 1024 * 1024},
+	{"Ki", 1024},
+}
+
+// NewFlag is used to initialise a new iecbyte.Flag with a default value
+//
+// A value of n that is < 0 will be set to 0
 func NewFlag(n int64) Flag {
+	if n < 0 {
+		n = 0
+	}
 	return Flag{n}
 }
 
 func (f *Flag) String() string {
-	for _, suffix := range order {
-		m := multipliers[suffix]
-		if f.n >= m && f.n%m == 0 {
-			return fmt.Sprintf("%d%s", f.n/m, suffix)
+	for _, m := range multipliers {
+		if f.n >= m.Value && f.n%m.Value == 0 {
+			return fmt.Sprintf("%d%s", f.n/m.Value, m.Suffix)
 		}
 	}
 
@@ -39,9 +46,9 @@ func (f *Flag) String() string {
 }
 
 func (f *Flag) Set(value string) error {
-	for _, suffix := range order {
-		if strings.HasSuffix(value, suffix) {
-			v := strings.TrimSuffix(value, suffix)
+	for _, m := range multipliers {
+		if strings.HasSuffix(value, m.Suffix) {
+			v := strings.TrimSuffix(value, m.Suffix)
 			n, err := strconv.ParseInt(v, 10, 64)
 			if err != nil {
 				return err
@@ -51,7 +58,7 @@ func (f *Flag) Set(value string) error {
 				return fmt.Errorf("cannot be negative")
 			}
 
-			f.n = n * multipliers[suffix]
+			f.n = n * m.Value
 
 			return nil
 		}
